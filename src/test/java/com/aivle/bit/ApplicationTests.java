edad45e623b2da21cf8d93fb28ed474c690bfe01
pwrwpw <dlgustjr9912@gmail.com>
2024-06-28 16:01:00 +0900

    🍎 Chore: 프로젝트 초기 설정

---
+++ src/test/java/com/aivle/bit/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivle.bit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
