f5133eecfc1fba516a493c10ed0c72ada237c1ad
b-sujin <sjsw123456@gmail.com>
2024-07-02 17:18:05 +0900

    ✨ Feat : 도메인 설계 - Member, Company, BaseTimeEntity

---
+++ src/main/java/com/aivle/bit/company/domain/Company.java
@@ -0,0 +1,23 @@
+package com.aivle.bit.company.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String industry;
+}
