67150c8dc5cc17f0d1afde876999457717225ef0
pwrwpw <dlgustjr9912@gmail.com>
2024-07-04 15:39:43 +0900

    ✨ Feat: 정규식을 통해 생성시 검증 하도록 수정

--- src/main/java/com/aivle/bit/member/domain/Member.java
+++ src/main/java/com/aivle/bit/member/domain/Member.java
@@ -1,21 +1,23 @@
 package com.aivle.bit.member.domain;
 
+import static com.aivle.bit.global.encode.PasswordEncoderSHA256.encode;
+import static com.aivle.bit.global.exception.ErrorCode.*;
+
 import com.aivle.bit.company.domain.Company;
-import com.aivle.bit.global.BaseTimeEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import com.aivle.bit.global.domain.BaseTimeEntity;
+import com.aivle.bit.global.exception.AivleException;
+import jakarta.persistence.*;
+import java.util.regex.Pattern;
 import lombok.Getter;
 import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter
 public class Member extends BaseTimeEntity {
+
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+    private static final String PASSWORD_REGEX = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?=\\S+$).{8,20}$";
+    private static final String NAME_REGEX = "^[가-힣]{2,10}$";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,9 +35,10 @@ public class Member extends BaseTimeEntity {
     @Column(nullable = false)
     private String address;
 
+    @Enumerated(EnumType.ORDINAL)
     @Column(nullable = false)
     @Comment("0-미인증, 1-일반 회원 인증, 2-관리자 인증, 3-일반 회원 휴먼, 4-관리자 휴먼")
-    private Short state;
+    private MemberState state;
 
     @Column(nullable = false, columnDefinition = "TINYINT(1)")
     @Comment("True-관리자, False-관리자 아님")
@@ -46,5 +49,47 @@ public class Member extends BaseTimeEntity {
     private Company company;
 
     protected Member() {
+    }
+
+    private Member(final String email, final String password, final String name, final String address,
+                   final MemberState state, final Boolean isAdmin, final Company company) {
+        this.email = email;
+        this.password = encode(password);
+        this.name = name;
+        this.address = address;
+        this.state = state;
+        this.isAdmin = isAdmin;
+        this.company = company;
+    }
+
+    public static Member of(final String email, final String password, final String name, final String address) {
+        validate(email, password, name, address);
+        return new Member(email, password, name, address, MemberState.UNVERIFIED, false, null);
+    }
+
+    private static void validate(final String email, final String password, final String name, final String address) {
+        if (!isValidEmail(email)) {
+            throw new AivleException(INVALID_EMAIL_FORMAT);
+        }
+
+        if (!isValidPassword(password)) {
+            throw new AivleException(INVALID_PASSWORD_FORMAT);
+        }
+
+        if (!isValidName(name)) {
+            throw new AivleException(INVALID_NAME_FORMAT);
+        }
+    }
+
+    private static boolean isValidEmail(final String email) {
+        return Pattern.matches(EMAIL_REGEX, email);
+    }
+
+    private static boolean isValidPassword(final String password) {
+        return Pattern.matches(PASSWORD_REGEX, password);
+    }
+
+    private static boolean isValidName(final String name) {
+        return Pattern.matches(NAME_REGEX, name);
     }
 }
