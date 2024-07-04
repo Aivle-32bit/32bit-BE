607cb37df7f57765df6d9f42a39264a699359a83
pwrwpw <dlgustjr9912@gmail.com>
2024-07-04 15:47:38 +0900

    ✨ Feat: SES 설정 추가

---
+++ src/main/java/com/aivle/bit/global/smtp/SesConfig.java
@@ -0,0 +1,33 @@
+package com.aivle.bit.global.smtp;
+
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SesConfig {
+
+    @Value("${ses.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${ses.credentials.secret-key}")
+    private String secretKey;
+
+    @Bean
+    AmazonSimpleEmailService amazonSimpleEmailService() {
+        return AmazonSimpleEmailServiceClientBuilder.standard()
+            .withCredentials(awsStaticCredentialsProvider())
+            .withRegion(Regions.AP_NORTHEAST_2)
+            .build();
+    }
+
+    private AWSStaticCredentialsProvider awsStaticCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey));
+    }
+}
