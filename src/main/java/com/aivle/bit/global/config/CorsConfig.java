2ae3e3e8ca49a74273c47b6e0f6421b3174aeddb
pwrwpw <dlgustjr9912@gmail.com>
2024-07-05 16:48:25 +0900

    ✨ Feat: localhost 도메인을 cors 허용하도록 수정

--- src/main/java/com/aivle/bit/global/config/CorsConfig.java
+++ src/main/java/com/aivle/bit/global/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOriginPatterns("http://127.0.0.1:3000")
+            .allowedOriginPatterns("http://127.0.0.1:3000", "http://localhost:3000")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .exposedHeaders("*")
