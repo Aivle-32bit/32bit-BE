72627efe08c6f1e29bfc0e0297fbc6c00601e69a
pwrwpw <dlgustjr9912@gmail.com>
2024-07-12 17:44:40 +0900

    ✨ Feat: 로그인 응답 데이터 추가

--- src/main/java/com/aivle/bit/auth/dto/response/SignInResult.java
+++ src/main/java/com/aivle/bit/auth/dto/response/SignInResult.java
@@ -4,12 +4,15 @@ import com.aivle.bit.member.domain.MemberState;
 
 public record SignInResult(
     String refreshToken,
+    Long memberId,
+    String memberName,
     MemberState state,
     boolean isAdmin
 ) {
 
     public static SignInResult from(SignInResponse signInResponse) {
-        return new SignInResult(signInResponse.tokenResponse().refreshToken(), signInResponse.state(),
+        return new SignInResult(signInResponse.tokenResponse().refreshToken(), signInResponse.memberId(),
+            signInResponse.memberName(), signInResponse.state(),
             signInResponse.isAdmin());
     }
 }
