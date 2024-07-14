343cc38bf88587146e8791411cd4377a7c412c31
pwrwpw <dlgustjr9912@gmail.com>
2024-07-14 22:08:06 +0900

    ✨ Feat: 개발 환경에선 secure 옵션 해제

--- src/main/java/com/aivle/bit/auth/dto/response/TokenResponse.java
+++ src/main/java/com/aivle/bit/auth/dto/response/TokenResponse.java
@@ -24,7 +24,7 @@ public record TokenResponse(String accessToken, String refreshToken) {
     public void setAccessToken(HttpServletResponse response, int expirationTime) {
         ResponseCookie accessTokenCookie = ResponseCookie.from(HttpHeaders.AUTHORIZATION, accessToken)
             .httpOnly(true)
-            .secure(true)
+//            .secure(true) 개발 환경에서는 주석 처리
             .path("/")
             .maxAge(expirationTime)
             .build();
