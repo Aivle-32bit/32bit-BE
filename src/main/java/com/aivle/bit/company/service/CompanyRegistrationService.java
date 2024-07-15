ac7faf14c767aa2294175fcaea5b3b86cee5bca9
pwrwpw <dlgustjr9912@gmail.com>
2024-07-15 14:50:20 +0900

    ✨ Feat: 유저 권한 설정 수정

--- src/main/java/com/aivle/bit/company/service/CompanyRegistrationService.java
+++ src/main/java/com/aivle/bit/company/service/CompanyRegistrationService.java
@@ -4,7 +4,10 @@ import com.aivle.bit.company.domain.CompanyRegistration;
 import com.aivle.bit.company.dto.request.CompanyRegistrationRequest;
 import com.aivle.bit.company.dto.response.CompanyRegistrationResponse;
 import com.aivle.bit.company.repository.CompanyRegistrationRepository;
+import com.aivle.bit.global.exception.AivleException;
+import com.aivle.bit.global.exception.ErrorCode;
 import com.aivle.bit.member.domain.Member;
+import com.aivle.bit.member.domain.MemberState;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +21,8 @@ public class CompanyRegistrationService {
     private final S3Service s3Service;
 
     public void save(CompanyRegistrationRequest request, Member member) {
+        validateMember(member);
+
         String key = s3Service.uploadImage(request.image(),
             "company-registration/image");
 
@@ -29,9 +34,16 @@ public class CompanyRegistrationService {
     }
 
     public List<CompanyRegistrationResponse> findAllByMember(Member member) {
+        validateMember(member);
         return companyRegistrationRepository.findAllByMember(member)
             .stream()
             .map(registration -> CompanyRegistrationResponse.from(registration, s3Service::generatePresignedUrl))
             .collect(Collectors.toList());
+    }
+
+    private void validateMember(Member member) {
+        if (member.getState() == MemberState.VERIFIED) {
+            throw new AivleException(ErrorCode.INVALID_REQUEST);
+        }
     }
 }
--- src/main/java/com/aivle/bit/global/config/AllowUnverifiedUserArgumentResolver.java
+++ src/main/java/com/aivle/bit/global/config/AllowUnverifiedUserArgumentResolver.java
@@ -44,16 +44,7 @@ public class AllowUnverifiedUserArgumentResolver implements HandlerMethodArgumen
         String jwtToken = extractJwtTokenFromCookies(webRequest);
         String email = jwtTokenProvider.extractEmailFromAccessToken(jwtToken);
 
-        Member memberByEmail = findMemberByEmail(email);
-
-        validateUnverifiedMemberState(memberByEmail);
-        return memberByEmail;
-    }
-
-    private void validateUnverifiedMemberState(Member member) {
-        if (member.getState() == MemberState.VERIFIED) {
-            throw new AivleException(INVALID_REQUEST);
-        }
+        return findMemberByEmail(email);
     }
 
     private String extractJwtTokenFromCookies(NativeWebRequest webRequest) {
