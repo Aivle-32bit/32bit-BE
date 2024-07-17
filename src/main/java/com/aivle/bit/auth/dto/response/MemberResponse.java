2811486583b8108b2336784f30794f2a2b47c6c7
pwrwpw <dlgustjr9912@gmail.com>
2024-07-17 13:25:46 +0900

    ✨ Feat: 회사 정보를 제공 하는 기능 추가

--- src/main/java/com/aivle/bit/auth/dto/response/MemberResponse.java
+++ src/main/java/com/aivle/bit/auth/dto/response/MemberResponse.java
@@ -9,17 +9,25 @@ public class MemberResponse {
 
     private final Long id;
     private final String name;
+    private final Long companyId;
     private final MemberState state;
     private final Boolean isAdmin;
 
-    private MemberResponse(Long id, String name, MemberState state, Boolean isAdmin) {
+    private MemberResponse(Long id, String name, Long companyId, MemberState state, Boolean isAdmin) {
         this.id = id;
         this.name = name;
+        this.companyId = companyId;
         this.state = state;
         this.isAdmin = isAdmin;
     }
 
     public static MemberResponse from(Member member) {
-        return new MemberResponse(member.getId(), member.getName(), member.getState(), member.getIsAdmin());
+        return new MemberResponse(
+            member.getId(),
+            member.getName(),
+            member.getCompany() != null ? member.getCompany().getId() : null,
+            member.getState(),
+            member.getIsAdmin()
+        );
     }
 }
