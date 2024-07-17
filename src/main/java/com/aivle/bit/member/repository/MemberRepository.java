6e8e5edeb74fb594690eb7aa996ee45bd68bb8b6
pwrwpw <dlgustjr9912@gmail.com>
2024-07-17 13:52:48 +0900

    🔨 Fix: 프록시 객체 접근할때 에러 발생 수정

--- src/main/java/com/aivle/bit/member/repository/MemberRepository.java
+++ src/main/java/com/aivle/bit/member/repository/MemberRepository.java
@@ -5,13 +5,14 @@ import com.aivle.bit.member.domain.MemberState;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    @Query("SELECT m FROM Member m LEFT JOIN FETCH m.company WHERE m.email = :email AND m.isDeleted = false")
     Optional<Member> findByEmailAndIsDeletedFalse(String email);
 
     boolean existsByEmailAndIsDeletedFalse(String email);
